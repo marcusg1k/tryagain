f520fea17f4254fa72858f8e7c6cb2b8e7f4350d
Marcus <marcusgutierrezufc@gmail.com>
2023-08-14 13:55:10 -0500

    UPDATED

--- app/src/main/java/com/project/tryagain/FoodListActivity.java
+++ app/src/main/java/com/project/tryagain/FoodListActivity.java
@@ -4,30 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class FoodListActivity extends AppCompatActivity {
 
-    private final String[] foodlist = {
-            "Apple (52 calories)",
-            "Banana (96 calories)",
-            "Orange (62 calories)",
-            "Grapes (69 calories)",
-            "Strawberry (4 calories)",
-            "Carrot (41 calories)"
-    };
+        private ListView foodListView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_foodlist);
+        private String[] foods = {
+                "Apple (52 calories)",
+                "Banana (96 calories)",
+                "Orange (62 calories)",
+                "Grapes (69 calories)",
+                "Strawberry (4 calories)",
+                "Carrot (41 calories)",
+                "Apple (52 calories)",
+                "Banana (96 calories)",
+                "Orange (62 calories)",
+                "Grapes (69 calories)",
+                "Strawberry (4 calories)",
+                "Carrot (41 calories)",
+                "Broccoli (55 calories)",
+                "Chicken Breast (165 calories)",
+                "Salmon (206 calories)",
+                "Brown Rice (215 calories)",
+                "Quinoa (222 calories)",
+                "Egg (68 calories)",
+                "Whole Wheat Bread (79 calories)",
+                "Spinach (7 calories)",
+                "Greek Yogurt (59 calories)",
+                "Almonds (7 calories per almond)",
+                "Oatmeal (150 calories)",
+                "Blueberries (84 calories)",
+                "Avocado (234 calories)",
+                "Cucumber (8 calories)",
+                "Tomato (5 calories)",
+                "Cheese (113 calories per slice)",
+                "Peanut Butter (94 calories per tablespoon)",
+                "Black Beans (114 calories)",
+                "Tuna (179 calories)",
+                "Cottage Cheese (206 calories)",
+                "Hummus (27 calories per tablespoon)",
+                "Turkey (135 calories per 3 oz)",
+                "Sweet Potato (112 calories)",
+                "Green Beans (31 calories per cup)",
+                "Mango (60 calories)",
+                "Pineapple (50 calories)",
+                "Watermelon (30 calories per cup)",
+                "Lentils (230 calories per cup)",
+                "Bell Pepper (30 calories)",
+                "Cauliflower (25 calories per cup)",
+                "Chia Seeds (58 calories per tablespoon)",
+                "Pomegranate (83 calories)",
+                "Walnuts (183 calories per 1/4 cup)",
+                "Cabbage (22 calories per cup)",
+                "Kiwi (41 calories)",
+                "Eggplant (20 calories per cup)",
+                "Brownie (approximately 180 calories)",
+                "Pasta (200 calories per cup)",
+                "Steak (250 calories per 4 oz)",
+                "Pork Chops (193 calories per 4 oz)",
+                "Pasta Salad (250 calories per cup)",
+                "Popcorn (30 calories per cup)",
+                "Chocolate Bar (around 200 calories)",
+                "Peanuts (166 calories per 1/4 cup)"
+        };
 
-        ListView foodListView = findViewById(R.id.foodListView);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            try {
+                setContentView(R.layout.activity_food_list);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getFoodlist());
-        foodListView.setAdapter(adapter);
+                foodListView = findViewById(R.id.foodListView);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foods);
+                foodListView.setAdapter(adapter);
+            } catch (Exception e) {
+                Toast Toast = null;
+                android.widget.Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-
-    public String[] getFoodlist() {
-        return foodlist;
-    }
-}
--- app/src/main/java/com/project/tryagain/MainActivity.java
+++ app/src/main/java/com/project/tryagain/MainActivity.java
@@ -8,28 +8,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            setContentView(R.layout.activity_main);
 
+            getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.badge));
 
-        TextView welcomeText = findViewById(R.id.welcomeText);
-        EditText nameInput = findViewById(R.id.nameInput);
-        EditText weightInput = findViewById(R.id.weightInput);
-        EditText ageInput = findViewById(R.id.ageInput);
-        EditText daysInput = findViewById(R.id.daysInput);
-        EditText goalWeightInput = findViewById(R.id.goalWeightInput);
+            TextView welcomeText = findViewById(R.id.welcomeText);
+            EditText nameInput = findViewById(R.id.nameInput);
+            EditText weightInput = findViewById(R.id.weightInput);
+            EditText daysInput = findViewById(R.id.daysInput);
+            EditText goalWeightInput = findViewById(R.id.goalWeightInput);
 
-        setContentView(R.layout.activity_main);
-        Button openFoodListButton = findViewById(R.id.openFoodListButton);
-        openFoodListButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FoodListActivity.class);
-                startActivity(intent);
-            }
-        });
+            Button openFoodListButton = findViewById(R.id.openFoodListButton);
+            openFoodListButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    try {
+                        Intent intent = new Intent(MainActivity.this, FoodListActivity.class);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "Error opening Food List", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
